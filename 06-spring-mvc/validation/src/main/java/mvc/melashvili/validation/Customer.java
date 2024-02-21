fed09eb42fb13e8ffec63055b0bd49bd2f0dec4c
NikaMelashvili <melashvilinikusha63@gmail.com>
2024-02-21 18:33:14 +0400

    spring validations

---
+++ 06-spring-mvc/validation/src/main/java/mvc/melashvili/validation/Customer.java
@@ -0,0 +1,28 @@
+package mvc.melashvili.validation;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "Is required")
+    @Size(min = 1, message = "Is required")
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
