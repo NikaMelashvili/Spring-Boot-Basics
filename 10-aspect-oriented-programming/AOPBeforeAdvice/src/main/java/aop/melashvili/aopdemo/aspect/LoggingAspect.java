6e8731542160d3fd1991ef341b6dc515581461ca
NikaMelashvili <melashvilinikusha63@gmail.com>
2024-02-27 18:29:17 +0400

    reusing pointcut expressions

--- 10-aspect-oriented-programming/AOPBeforeAdvice/src/main/java/aop/melashvili/aopdemo/aspect/LoggingAspect.java
+++ 10-aspect-oriented-programming/AOPBeforeAdvice/src/main/java/aop/melashvili/aopdemo/aspect/LoggingAspect.java
@@ -2,13 +2,17 @@ package aop.melashvili.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* add* (aop.melashvili.aopdemo.Account))")
+    @Pointcut("execution(* add* (aop.melashvili.aopdemo.Account, ..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void log(){
         System.out.println("This is a Before logger");
     }
