cfb9074f17061eac974b99acdb87791e9c6378b6
NikaMelashvili <melashvilinikusha63@gmail.com>
2024-11-09 15:15:27 +0400

    atomic persistance

---
+++ Playgound/atomicPersistence/src/test/java/com/melashvili/atomicpersistence/AtomicPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melashvili.atomicpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
